5bad3ec6195fa3eddd58247fc42f803350d81954
Amarigh <60605851+Amarigh@users.noreply.github.com>
2020-04-04 19:40:31 +0100

    Add files via upload

---
+++ devoirs_php/devoir2_php/exercice1.php
@@ -0,0 +1,34 @@
+
+-----------------------------------------------
+
+binom:  
+           Amarigh Mustapha G1
+           Farid Elkharrazi G2
+
+
+
+------------------ Exercice 1---------------------
+
+
+
+
+
+
+ <?php 
+
+function explode_($string,$car){
+	$str='%['.$car.']%';
+  
+ 
+
+     return preg_split($str,$string);
+
+     // return explode($car,$string);
+
+}
+
+
+
+ ?>  
+
+ 
---
+++ devoirs_php/devoir2_php/exercice3_Validation.php
@@ -0,0 +1,52 @@
+
+
+
+-----------------------------------------------
+
+binom:  
+           Amarigh Mustapha G1
+           Farid Elkharrazi G2
+
+
+
+------------------ Exercice 3 (partie 2)---------------------
+
+
+
+
+
+<?php 
+
+
+if(isset($_POST['ok'])){
+
+
+
+
+	
+}
+
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title></title>
+ </head>
+ <body>
+ <h1>validation de la date </h1>
+<p>la date date saisir est : <b><?php echo $_POST['sl1']."/".$_POST['sl2']."/".$_POST['sl3']; ?></b></p>
+
+<?php 
+
+if($_POST['sl3']>=2000){
+?>
+<b>La date saisie  <span style="color: green;">est valide</span></b>
+<?php }
+else { ?>
+	<b>l'anne <?php echo $_POST['sl3']; ?> est non bissextile :  <span style="color: red;">Date invalide</span></b>
+<?php } ?>
+
+ </body>
+ </html>
