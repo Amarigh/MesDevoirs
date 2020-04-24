131fe41ac6349b49dc5905be393f7ea8fdd911c4
Amarigh <60605851+Amarigh@users.noreply.github.com>
2020-04-24 01:41:33 +0000

    Add files via upload

---
+++ site web de commerce/Code source de notre site web de commerce/index.php
@@ -0,0 +1,233 @@
+<?php
+session_start();
+include('layoutfirst.php')
+?>
+
+
+
+
+<!-- melieu -->
+
+
+
+
+<section id="home-section" class="hero">
+  <div class="home-slider js-fullheight owl-carousel">
+    <div class="slider-item js-fullheight">
+      <div class="overlay"></div>
+      <div class="container-fluid p-0">
+        <div class="row d-md-flex no-gutters slider-text js-fullheight align-items-center justify-content-end" data-scrollax-parent="true">
+          <div class="one-third order-md-last img js-fullheight" style="background-image:url(images/bg_1.jpg);">
+          </div>
+          <div class="one-forth d-flex js-fullheight align-items-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+            <div class="text">
+              <span class="subheading"> eCommerce Vêtments</span>
+              <div class="horizontal">
+                <h3 class="vr">Etabli depuis 2020</h3>
+                <h1 class="mb-4 mt-3">Attraper le vôtre <br><span>Style &amp; Regardez</span></h1>
+                <p> une petite rivière nommée Duden coule à leur place et lui fournit les regelialia nécessaires. C'est un pays paradisiaque.</p>
+
+                <p><a href="produit.php" class="btn btn-primary px-5 py-3 mt-3">Achetez maintenant</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="slider-item js-fullheight">
+      <div class="overlay"></div>
+      <div class="container-fluid p-0">
+        <div class="row d-md-flex no-gutters slider-text js-fullheight align-items-center justify-content-end" data-scrollax-parent="true">
+          <div class="one-third order-md-last img js-fullheight" style="background-image:url(images/product-8.jpg);">
+          </div>
+          <div class="one-forth d-flex js-fullheight align-items-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+            <div class="text">
+              <span class="subheading"> eCommerce vêtments</span>
+              <div class="horizontal">
+                <h3 class="vr" style="background-image: url(images/divider.jpg);">Etabli depuis 2020</h3>
+                <h1 class="mb-4 mt-3">Attrapez le vôtre <br><span>Style &amp; Regardez</span></h1>
+                <p>une petite rivière nommée Duden coule à leur place et lui fournit les regelialia nécessaires. C'est un pays paradisiaque.</p>
+
+                <p><a href="produit.php" class="btn btn-primary px-5 py-3 mt-3">Achetez maintenant</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+    <div class="slider-item js-fullheight">
+      <div class="overlay"></div>
+      <div class="container-fluid p-0">
+        <div class="row d-md-flex no-gutters slider-text js-fullheight align-items-center justify-content-end" data-scrollax-parent="true">
+          <div class="one-third order-md-last img js-fullheight" style="background-image:url(images/image_1.jpg);">
+          </div>
+          <div class="one-forth d-flex js-fullheight align-items-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+            <div class="text">
+              <span class="subheading"> eCommerce vêtments</span>
+              <div class="horizontal">
+                <h3 class="vr" style="background-image: url(images/divider.jpg);">Etabli depuis 2020</h3>
+                <h1 class="mb-4 mt-3">Attrapez le vôtre <br><span>Style &amp; Regardez</span></h1>
+                <p>une petite rivière nommée Duden coule à leur place et lui fournit les regelialia nécessaires. C'est un pays paradisiaque..</p>
+
+                <p><a href="shop.html" class="btn btn-primary px-5 py-3 mt-3">shop Now</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+    <div class="slider-item js-fullheight">
+      <div class="overlay"></div>
+      <div class="container-fluid p-0">
+        <div class="row d-flex no-gutters slider-text js-fullheight align-items-center justify-content-end" data-scrollax-parent="true">
+          <div class="one-third order-md-last img js-fullheight" style="background-image:url(images/bg_2.jpg);">
+          </div>
+          <div class="one-forth d-flex js-fullheight align-items-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+            <div class="text">
+              <span class="subheading">eCommerce vêtments</span>
+              <div class="horizontal">
+                <h3 class="vr" style="background-image: url(images/divider.jpg);">meilleure eCommerce enligne boutique</h3>
+                <h1 class="mb-4 mt-3">une bien <span>Moderne</span> Femme</h1>
+                <p>Une petite rivière nommée Duden coule à leur place et lui fournit les regelialia nécessaires. C'est un pays paradisiaque.</p>
+
+                <p><a href="produit.php" class="btn btn-primary px-5 py-3 mt-3">Achetez m intenaint</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+<!-- fin de melieu -->
+<br>
+<!-- exemple de produit -->
+<section class="ftco-section ftco-choose ftco-no-pb ftco-no-pt">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 d-flex align-items-stretch">
+        <div class="img" style="background-image: url(images/about-1.jpg);"></div>
+      </div>
+      <div class="col-md-4 py-md-5 ftco-animate">
+        <div class="text py-3 py-md-5">
+          <h2 class="mb-4">Nouvelle collection d'été de vêtements pour les femmes 2020 </h2>
+          <p>loin, derrière le mot montagnes, loin des pays Vokalia et Consonantia, vivent les textes aveugles. Séparés, ils vivent dans Bookmarksgrove, juste sur la côte de la Sémantique, un grand océan linguistique.</p>
+          <p><a href="produit.php" class="btn btn-white px-4 py-3">Achetez maintenaint</a></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-5 order-md-last d-flex align-items-stretch">
+        <div class="img img-2" style="background-image: url(images/about-2.jpg);"></div>
+      </div>
+      <div class="col-md-7 py-3 py-md-5 ftco-animate">
+        <div class="text text-2 py-md-5">
+          <h2 class="mb-4">Nouvelle collection d'été de vêtements pour les hommes 2020</h2>
+          <p>loin, derrière le mot montagnes, loin des pays Vokalia et Consonantia, vivent les textes aveugles. Séparés, ils vivent dans Bookmarksgrove, juste sur la côte de la Sémantique, un grand océan linguistique.</p>
+          <p><a href="produit.php" class="btn btn-white px-4 py-3">achetez maintenaint</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<!-- fin d'exemple de produit -->
+<br>
+<!-- exemple 2 -->
+
+
+<section class="ftco-section ftco-counter img" id="section-counter" style="background-image: url(images/bg_4.jpg);">
+  <div class="container">
+    <div class="row justify-content-center py-5">
+      <div class="col-md-10">
+        <div class="row">
+          <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18 text-center">
+              <div class="text">
+                <strong class="number" data-number="10000">0</strong>
+                <span>Clients satisfaits</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18 text-center">
+              <div class="text">
+                <strong class="number" data-number="100">0</strong>
+                <span>Branches</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18 text-center">
+              <div class="text">
+                <strong class="number" data-number="1000">0</strong>
+                <span>Partenaire</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18 text-center">
+              <div class="text">
+                <strong class="number" data-number="100">0</strong>
+                <span>Prix</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- fin d'exemple 2 -->
+<!-- subscride -->
+
+
+
+<section class="ftco-section-parallax">
+  <div class="parallax-img d-flex align-items-center">
+    <div class="container">
+      <div class="row d-flex justify-content-center py-5">
+        <div class="col-md-7 text-center heading-section ftco-animate">
+          <h2>Abonnez-vous à notre newsletter</h2>
+          <div class="row d-flex justify-content-center mt-5">
+            <div class="col-md-8">
+              <form action="#" class="subscribe-form">
+                <div class="form-group d-flex">
+                  <input type="text" class="form-control" placeholder="Entrez votre mail">
+                  <input type="submit" value="Subscribe" class="submit px-3">
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- fin de subscribe -->
+
+
+<?php include('layoutlast.php') ?>
